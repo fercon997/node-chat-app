7c5e4cb5377b01ebe0e60eee68a2385d927aa646
fercon997 <fercon997@gmail.com>
2018-03-10 17:12:52 -0400

    Add message basics

--- server/server.js
+++ server/server.js
@@ -15,6 +15,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+//EJEMPLOS
+  // socket.emit('newEmail', {
+  //   from: 'fercon@example.com',
+  //   text: 'Qlq',
+  //   createAt: 12873
+  // });
+
+  // socket.emit('newMessage', {
+  //   from: "asdka@gmai.com",
+  //   text: "Qlq bruja",
+  //   createdAt: 212
+  // });
+
+ // socket.on('createEmail', (newEmail) => {
+ //   console.log('createEmail', newEmail);
+ // })
+//FIN DE EJEMPLOS
+
+ socket.on('createMessage', (message) => {
+   console.log('createMessage', message);
+   io.emit('newMessage', {
+    from: message.from,
+    text: message.text,
+    createdAt: new Date().getTime()
+   });
+ });
+
   socket.on('disconnect', () => {
     console.log("Client disconnected");
   });
