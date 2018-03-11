9374ce43c0237b288c7db201f381914a6328398a
fercon997 <fercon997@gmail.com>
2018-03-11 17:23:47 -0400

    Add jQuery and forms

--- public/js/index.js
+++ public/js/index.js
@@ -18,14 +18,21 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
-socket.on('welcomeMessage', function(message){
-  console.log('welcomeMessage', message);
-});
-
-socket.on('welcomeMessageAll', function(message) {
-  console.log('welcomeMessageAll', message);
-})
-
 socket.on('newMessage', function(message){
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+
+
+jQuery('#message-form').on('submit', function (e){
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {});
 });
